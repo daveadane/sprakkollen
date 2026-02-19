f8899e4d7521049267a43285bffb1feb62145ab3
Dawit Adane <dawitadane.woldekirkose@yh.nackademin.se>
2026-02-19 08:07:13 +0100

    modifiying frontend vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), tailwindcss(),
+    react(),
+    tailwindcss(),
   ],
-})
+  server: {
+    port: 5173,
+    strictPort: true, // ❗ fail instead of switching to 5174
+  },
+});
+
